4600f4c6a75404536a63b373d9bea5a05761e337
cam45819 <callum.a.mcmahon@gsk.com>
2025-01-27 20:58:42 +0000

    add gcloud to work profile

--- modules/work-settings.nix
+++ modules/work-settings.nix
@@ -1,8 +1,15 @@
 {
   pkgs,
   lib,
+  specialArgs,
   ...
-}: {
+}: let
+  gdk = pkgs.google-cloud-sdk.withExtraComponents( with pkgs.google-cloud-sdk.components; [
+      gke-gcloud-auth-plugin
+    ]);
+in
+{
   # https://github.com/NixOS/nix/issues/8081
   nix.settings.ssl-cert-file = /etc/nix/ca_cert.pem;
+  home-manager.users.${specialArgs.username}.home.packages = [gdk];
 }
