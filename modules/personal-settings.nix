ba590dc73a2f851382df75bd54dcd7658d7e003c
callum <mcmahon.callum@gmail.com>
2025-02-26 23:19:21 +0000

    add tailscale to personal services

--- modules/personal-settings.nix
+++ modules/personal-settings.nix
@@ -1,10 +1,19 @@
 {
   pkgs,
+  pkgs-unstable,
   specialArgs,
   ...
 }:
 {
   home-manager.users.${specialArgs.username} = {
-    home.packages = [pkgs.signal-desktop];
+    home.packages = [
+      pkgs.signal-desktop 
+      pkgs-unstable.whatsapp-for-mac 
+      pkgs-unstable.raycast
+    ];
+  };
+  services.tailscale = {
+    enable = true;
+    package = pkgs-unstable.tailscale;
   };
 }
